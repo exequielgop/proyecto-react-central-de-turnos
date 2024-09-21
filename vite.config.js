630b5938c69ee076fde68d2c36d5d9e7871c6372
Exequiel Goapper <exequielgop@gmail.com>
2024-09-21 01:43:55 -0300

    Delete vite.config comments

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,9 @@ const hrefBasePath = () => {
   let basePath;
   return {
     name: "vite-plugin-href-base-path",
-    // https://vitejs.dev/guide/api-plugin.html#configresolved
     configResolved(config) {
       basePath = config.base;
     },
-    // https://vitejs.dev/guide/api-plugin.html#transformindexhtml
     transformIndexHtml(html) {
       const hrefRE = /(?<=<a\b[^>]+href\s*=\s*["'])\//gi;
       return html.replace(hrefRE, basePath);
